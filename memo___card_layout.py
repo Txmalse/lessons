7d3a207efccf97ed6efb5d81533d04ffe14ab1ae
Maksym Maksymenko <147704774+Txmalse@users.noreply.github.com>
2024-05-09 14:11:27 +0300

    Update memo___card_layout.py

--- memo___card_layout.py
+++ memo___card_layout.py
@@ -7,26 +7,96 @@ from PyQt5.QtWidgets import (
         QHBoxLayout, QVBoxLayout, 
         QGroupBox, QButtonGroup, QRadioButton,  
         QPushButton, QLabel, QSpinBox)
-from memo___app import app 
+from memo_app import app 
 
-# віджети, які треба буде розмістити:
-# кнопка повернення в основне вікно 
-# кнопка прибирає вікно і повертає його після закінчення таймера
-# введення кількості хвилин
-# кнопка відповіді "Ок" / "Наступний"
-# текст питання
+app = QApplication([])
 
-# Опиши групу перемикачів
+btn_Menu = QPushButton('Меню')
+btn_Sleep = QPushButton('Відпочити')
+box_Minutes = QSpinBox()
+box_Minutes.setValue(30)
+btn_OK = QPushButton('Відповісти')
+lb_Question = QLabel('')
 
-# Опиши панень з результатами
+RadioGroupBox = QGroupBox('Варіанти відповіді')
+RadioGroup = QButtonGroup()
 
-# Розмісти весь вміст в лейаути. Найбільшим лейаутом буде layout_card
+rbtn_1 = QRadioButton('')
+rbtn_2 = QRadioButton('')
+rbtn_3 = QRadioButton('')
+rbtn_4 = QRadioButton('')
 
-# Результат роботи цього модуля: віджети поміщені всередину layout_card, який можна призначити вікну.
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+AnsGroupBox = QGroupBox('Результати тесту')
+lb_Result = QLabel('')
+lb_Correct = QLabel('')
+
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+
+layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_2)
+
+layout_ans3.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_4)
+
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+
+RadioGroupBox.setLayout(layout_ans1)
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+
+AnsGroupBox.setLayout(layout_res)
+AnsGroupBox.hide()
+
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+layout_line4 = QHBoxLayout()
+
+layout_line1.addWidget(btn_Menu)
+layout_line1.addStretch(1)
+
+layout_line1.addWidget(btn_Sleep)
+layout_line1.addWidget(box_Minutes)
+layout_line1.addWidget(QLabel('хвилин'))
+
+layout_line2.addWidget(lb_Question, alignment=(Qt.AlignCenter | Qt.AlignVCenter))
+layout_line3.addWidget(RadioGroupBox)
+layout_line3.addWidget(AnsGroupBox)
+
+layout_line4.addStretch(1)
+layout_line4.addWidget(btn_OK,stretch=2, alignment=(Qt.AlignVCenter))
+layout_line4.addStretch(1)
+
+layout_card = QVBoxLayout()
+
+layout_card.addLayout(layout_line1,stretch=1)
+layout_card.addLayout(layout_line2,stretch=2)
+layout_card.addLayout(layout_line3,stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line4, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # прогалини між вмістом
+
+
+# Результат роботи цього модуля: віджети розміщені всередині layout_card, який можна призначити вікну.
+
+
 def show_result():
-    ''' показати панель відповідей '''
-    pass
+   pass
 
 def show_question():
-    ''' показати панель запитань '''
-    pass
+   pass
+
+
+
+
+
